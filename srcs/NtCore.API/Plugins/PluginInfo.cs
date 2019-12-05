afebd53bfe83f93c000795e7d0801ab973910c58
Roxeez <roxeeztsu@gmail.com>
2019-12-05 17:33:02 +0100

    Add plugin info

---
+++ srcs/NtCore.API/Plugins/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NtCore.API.Plugins
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class PluginInfo : Attribute
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public bool IsInjected { get; set; }
+    }
+}
