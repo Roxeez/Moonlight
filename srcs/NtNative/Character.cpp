6bd239fb955122cbe03d027eeeeceedb76459683
Roxeez <roxeeztsu@gmail.com>
2019-12-22 06:57:14 +0100

    Git kraken :pepega:

--- srcs/NtNative/Character.cpp
+++ srcs/NtNative/Character.cpp
@@ -3,15 +3,14 @@
 //
 
 #include "Character.hpp"
-#include "PatternScanner.hpp"
 
 DWORD _walkObject;
 DWORD _walkFunction;
 
-void Character::initialize()
+void Character::initialize(Module module)
 {
-    _walkObject = *(DWORD*)(FindPattern((uint8_t*)0x401000, 0x2d1000, (uint8_t*)"\x33\xC9\x8B\x55\xFC\xA1\x00\x00\x00\x00\xE8\x00\x00\x00\x00", "xxxxxx????x????") + 0x6);
-    _walkFunction = (DWORD)FindPattern((uint8_t*)0x401000, 0x2d1000, (uint8_t*)"\x55\x8B\xEC\x83\xC4\xEC\x53\x56\x57\x66\x89\x4D\xFA", "xxxxxxxxxxxxx");
+    _walkObject = *(DWORD*)(module.findPattern<DWORD>("\x33\xC9\x8B\x55\xFC\xA1\x00\x00\x00\x00\xE8\x00\x00\x00\x00", "xxxxxx????x????") + 0x6);
+    _walkFunction = module.findPattern<DWORD>("\x55\x8B\xEC\x83\xC4\xEC\x53\x56\x57\x66\x89\x4D\xFA", "xxxxxxxxxxxxx");
 }
 
 void Character::walk(short x, short y)
