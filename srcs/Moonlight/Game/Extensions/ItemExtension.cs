fc30e42440f6c3b1adf946b09fa26f077a708289
Roxeez <roxeeztsu@gmail.com>
2020-02-10 03:41:31 +0100

    Add some extension

--- srcs/Moonlight/Game/Extensions/ItemExtension.cs
+++ srcs/Moonlight/Game/Extensions/ItemExtension.cs
@@ -1,4 +1,5 @@
 ﻿using Moonlight.Core.Enums;
+using Moonlight.Game.Inventories;
 using Moonlight.Game.Inventories.Items;
 
 namespace Moonlight.Game.Extensions
@@ -8,5 +9,9 @@ namespace Moonlight.Game.Extensions
         public static bool IsPotion(this Item item) => item.BagType == BagType.MAIN && item.Type == 5 && item.SubType == 0;
         public static bool IsFood(this Item item) => item.BagType == BagType.ETC && item.Type == 1 && item.SubType == 0;
         public static bool IsSnack(this Item item) => item.BagType == BagType.ETC && item.Type == 2 && item.SubType == 0;
+
+        public static bool IsPotion(this ItemInstance item) => item.Item.IsPotion();
+        public static bool IsFood(this ItemInstance item) => item.Item.IsFood();
+        public static bool IsSnack(this ItemInstance item) => item.Item.IsSnack();
     }
 }
